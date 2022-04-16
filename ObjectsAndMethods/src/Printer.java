37fb38506cfbf4cb5858a2c6c1f34c8ab9bf4f0d
maksim_beztsennyi <maximka93@list.ru>
2022-04-17 00:29:59 +0300

    exercise 3, homework 4.10

---
+++ ObjectsAndMethods/src/Printer.java
@@ -0,0 +1,49 @@
+public class Printer {
+
+    private static String queue = "";
+    private static int pendingPagesCount = 0;
+    private static int countPrintedPages;
+
+    public static void main(String[] args) {
+        append("Введение");
+        append("Курсовая работа", "Титульный лист");
+        append("История Царской России 1701-1917 годы", "Содержание работы", 16);
+        print("Документы ожидающие печать: ");
+    }
+
+    public static void append(String content) {
+        append(content, "", 1);
+    }
+
+    public static void append(String content, String name) {
+        append(content, name, 1);
+    }
+
+    public static void append(String content, String name, int page) {
+        queue = queue + "\n" + content + " " + name + " " + "Количество страниц: " + page;
+        pendingPagesCount = pendingPagesCount + page;
+    }
+
+    public static int getPendingPagesCount() {
+        return pendingPagesCount;
+    }
+    public static int getCountPrintedPages() {
+        return pendingPagesCount;
+    }
+
+    public static void clear() {
+        queue = "";
+    }
+
+    public static void print(String loading) {
+        System.out.println(loading);
+        if(queue.isEmpty()) {
+            System.out.println("Отсутствуют документы в очереди на печать!!!");
+        } else {
+            System.out.println(queue);
+            System.out.println("Общее количество страниц ожидающих печать: " + getPendingPagesCount());
+            System.out.println("Количество напечатанных страниц: " + getCountPrintedPages());
+            clear();
+        }
+    }
+}
