5dc604ade40ed17fc3c63ba951bd08a27885138d
maksim_beztsennyi <maximka93@list.ru>
2022-05-01 02:29:38 +0300

    fix (i <= 65535)

--- PrimitiveDataTypes/IntOrInteger/src/Main.java
+++ PrimitiveDataTypes/IntOrInteger/src/Main.java
@@ -1,14 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         Container container = new Container();
+        System.out.println(container.getCount());
         container.addCount(5672);
+        System.out.println(container.getCount());
+        container.addCount(1000);
         System.out.println(container.getCount());
 
         // TODO: ниже напишите код для выполнения задания:
         //  С помощью цикла и преобразования чисел в символы найдите все коды
         //  букв русского алфавита — заглавных и строчных, в том числе буквы Ё.
 
-        for (int i = 0; i <= 65536; i++) {
+        for (int i = 0; i <= 65535; i++) {
             char lettersRussian = (char) i;
             if (lettersRussian >= 'а' && lettersRussian <= 'я')
                 System.out.println(lettersRussian + "-" + i);
